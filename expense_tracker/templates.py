e0429b0ac29698122a8bcd0ac8c5da082e812644
Developer <user@example.com>
2026-07-06 23:05:58 +0530

    Position Loops tab navigation at the bottom of the sidebar list

--- expense_tracker/templates.py
+++ expense_tracker/templates.py
@@ -744,11 +744,6 @@ def page(
           <svg class="nav-icon" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
           <span>Import / Add</span>
         </a>
-        <a href="#loops" class="tab-link" data-tab="loops">
-          <svg class="nav-icon" viewBox="0 0 24 24"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>
-          <span>Loops</span>
-          {f'<span class="tab-badge ok" id="suggestions-badge">{len(data["suggestions"])}</span>' if len(data["suggestions"]) > 0 else ""}
-        </a>
         <a href="#review" class="tab-link" data-tab="review">
           <svg class="nav-icon" viewBox="0 0 24 24"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2zm0-4H7V7h10v2zm0 8H7v-2h10v2z"/></svg>
           <span>Review Queue</span>
@@ -765,6 +760,11 @@ def page(
         <a href="#rules" class="tab-link" data-tab="rules">
           <svg class="nav-icon" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/></svg>
           <span>Knowledge & Shared</span>
+        </a>
+        <a href="#loops" class="tab-link" data-tab="loops">
+          <svg class="nav-icon" viewBox="0 0 24 24"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>
+          <span>Loops</span>
+          {f'<span class="tab-badge ok" id="suggestions-badge">{len(data["suggestions"])}</span>' if len(data["suggestions"]) > 0 else ""}
         </a>
       </nav>
     </aside>
